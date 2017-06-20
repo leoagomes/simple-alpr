28e02daae7927c513c82350ada3279c909f5bacd
Leonardo <lgomes.leo@gmail.com>
2017-06-20 12:07:30 -0300

    Further changes

--- lpr.py
+++ lpr.py
@@ -1,17 +1,32 @@
 import cv2
 import numpy as np
 import sys
+
 from PIL import Image
 import pytesseract
 
+# scale up dimensions (400 * 3, 130 * 3) aka BR plate dimensions * 3
+plate_image_dimensions = (1200,390)
+
 def lpr(image, show_steps = False):
+	# pre-process the image
 	ppimg = pre_process(image)
+
+	# run through edge detection algorithm
 	edgeimg = detect_edges(ppimg)
+
+	# get the region where the license plate is
 	(out, approx, cnt) = find_license_plate(edgeimg)
 
-	(prx, pry, prw, prh) = cv2.boundingRect(approx)
-	plate = ppimg[prx:prx+prw, pry:pry+prh].copy()
+	# get a separate, resized, license plate image
+	plate = separate_resize_plate(ppimg, out, approx, cnt)
 
+	# make the plate binary
+	binplate = binarize_plate(plate)
+
+	helper_showwait("binplate", binplate)
+
+	# show steps if the flag is set
 	if show_steps:
 		helper_imshow("Original Image", image)
 		helper_imshow("Pre-Processed Image", ppimg)
@@ -28,10 +43,37 @@ def lpr(image, show_steps = False):
 
 	return "NOT AVAILABLE"
 
+def separate_resize_plate(image, out, apr, cnt):
+	# Create an image containing only the plate
+	cleanimg = image.copy()
+	mask = np.full_like(cleanimg, 255)
+	cv2.drawContours(mask, [cnt], 0, (0, 0, 0), -1)
+	cv2.drawContours(mask, [cnt], 0, (255, 255, 255), 2)
+	cleanimg = cv2.add(cleanimg, mask)
+
+	# work on the plate region
+	(prx, pry, prw, prh) = cv2.boundingRect(apr)
+	plate = cleanimg[pry:pry+prh, prx:prx+prw].copy()
+
+	# Resize the plate
+	plate = cv2.resize(plate, plate_image_dimensions)
+	return pre_process(plate)
+
+def binarize_plate(plate):
+	#result = cv2.adaptiveThreshold(plate, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+		#cv2.THRESH_BINARY, 11, 2)
+
+	_, result = cv2.threshold(plate, 127, 255, cv2.THRESH_BINARY)
+	return result
+
+def calculate_Otsu(image):
+	histogram = cv2.calcHist([image])
+
 def pre_process(image):
 	# enhance image contrast
 	img = pp_enhance_contrast(image)
 
+	# TODO: REMOVE AS NOT NEEDED, CANNY ALREADY DOES GAUSSIAN FILTERING
 	# try and remove noise using a Gaussian Filter
 	#img = cv2.GaussianBlur(img, (5,5), 0)
 
@@ -57,12 +99,13 @@ def detect_edges(image):
 	return out
 
 def find_license_plate(image):
-	area_threshold = 2000
+	area_threshold = 2000 # arbitrary threshold for plate area in image.
 	img, contours, h = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 	for contour in contours:
 		approx = cv2.approxPolyDP(contour, cv2.arcLength(contour, True) * 0.05, True)
 
+		# find contours with 4 edges and the area of which is greater than threshold
 		if len(approx) == 4 and np.abs(cv2.contourArea(contour)) > area_threshold:
 			rect = cv2.minAreaRect(contour)
 			box = np.int0(cv2.boxPoints(rect))
@@ -70,9 +113,10 @@ def find_license_plate(image):
 
 			ratio = box_w / box_h
 
+			# brazilian license plate is 400mm x 130mm
 			# 400 / 130 ~= 3.07... accept +- 0.3 error
 			if 2.7 < ratio and ratio < 3.4:
-				# TODO: check approx
+				# TODO: check approx is rectangle, if not "continue" the for loop
 
 				# debug
 				#cv2.drawContours(img, [box, contour, approx], -1, (0,0,255), 2)
@@ -118,7 +162,7 @@ if len(sys.argv) < 2:
 
 image = cv2.imread(str(sys.argv[1]), cv2.IMREAD_GRAYSCALE)
 
-# TODO: remove
+# TODO: remove // OR NOT (???), NORMALIZES IMAGE SIZES
 image = cv2.resize(image, (1280, 720))
 
 # Check the show steps argument
@@ -126,4 +170,5 @@ show_steps = False
 if len(sys.argv) > 2:
 	show_steps = (int(sys.argv[2]) == 1)
 
+# print final result (plate's number)
 print(lpr(image, show_steps))
